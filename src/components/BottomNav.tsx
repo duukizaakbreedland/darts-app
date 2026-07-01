564848b34661ff930843a4b1ea6677a3d1029082
duukizaakbreedland <duuk.izaak@gmail.com>
2026-07-01 10:56:22 +0200

    BottomNav: knoppen iets lager (h-14 -> h-11)

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -18,7 +18,7 @@ export function BottomNav() {
           <button
             key={path}
             onClick={() => navigate(path)}
-            className="relative flex-1 flex items-center justify-center h-14 transition-colors"
+            className="relative flex-1 flex items-center justify-center h-11 transition-colors"
           >
             {active && (
               <span className="absolute top-0 left-1/2 -translate-x-1/2 w-10 h-0.5 bg-blue-500 rounded-full" />
